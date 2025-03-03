f2fef12ef2ec905ba0a476c8163568503b8043d9
GleidesonFr <gleidesonfreitas015@gmail.com>
2025-03-03 12:11:45 -0300

    first commit

---
+++ backend/serviceflow/src/test/java/br/com/serviceflow/ServiceflowApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.serviceflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
